edffc2968d83943784c1eb479fc538a19377f8d1
John Dias <120695464+JohnDias0@users.noreply.github.com>
2023-09-18 22:32:46 -0300

    Update DataBaseConectionFactory.java

--- dsI/auladejava/aula-4/aula-4/src/main/java/br/eteczl/persistencce/DataBaseConectionFactory.java
+++ dsI/auladejava/aula-4/aula-4/src/main/java/br/eteczl/persistencce/DataBaseConectionFactory.java
@@ -1,7 +1,25 @@
-package br.eteczl.persistencce;
+package ProjGradleBD.persistence;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-public class DataBaseConectionFactory {
-    Connection conn;
+public class ConectionFact {
+    
+    private Connection conn = null;
+    private String StringDeConexao = "jbc:mysql://localhost:3306/etec";
+
+    public Connection getConnection(){
+    
+        try {
+            DriverManager.getConnection(this.StringDeConexao, "root", "");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return this.conn;
+    
+    }
 }
+    
+
+ 
